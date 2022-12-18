1a87316c9fbcf6fd53e9ecf92ea152c26bcd2a46
t_iwazaki <t_iwazaki@imin.co.jp>
2022-12-18 15:20:46 +0900

    mod dburl

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/api/v1/tasks",taskRoute)
 //データベースと接続
 const start = async ()=>{
   try{
-    await connectDB(process.env.MONGO_URL)
+    await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL)
     app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
   } catch (err){
     console.log(err);
